61be78cba9e71ab4ab7d44d9b4a8719f2cdeea2b
Thomas Laporte <thomas.laporte03@gmail.com>
2016-12-16 16:08:32 +0100

    Add files via upload
    Ajout de la requête préparée

--- login.php
+++ login.php
@@ -1,15 +1,21 @@
 <?php
   include('bddConnection.php');
-  $sql = "SELECT * FROM PEPSI2.users WHERE login='".$_POST['login']."' AND password='".sha1($_POST['psw'])."' LIMIT 1;";
-  $sth = $dbh->query($sql);
-  $rowset = $sth->fetch(PDO::FETCH_ASSOC);
+  // $sql = "SELECT * FROM PEPSI2.users WHERE login='".$_POST['login']."' AND password='".sha1($_POST['psw'])."' LIMIT 1;";
+  // $sth = $dbh->query($sql);
+  // $rowset = $sth->fetch(PDO::FETCH_ASSOC);
 
-  session_start();
-  if(count($rowset) > 1)
-  {
-    $_SESSION['user'] = $rowset;
-  }
-  else {
-    $_SESSION['error'] = 1;
-  }
-  header('Location:index.php');
+
+  $sql = "SELECT * FROM PEPSI2.users WHERE login = :login AND password = :pswd LIMIT 1;";
+  $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  $sth->execute(array(':login' => $_POST['login'], ':pswd' => sha1($_POST['psw'])));
+  $rowset = $sth->fetch();
+
+    session_start();
+    if(count($rowset) > 1)
+    {
+      $_SESSION['user'] = $rowset;
+    }
+    else {
+      $_SESSION['error'] = 1;
+    }
+    header('Location:index.php');
