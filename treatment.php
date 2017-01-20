0d495fce39b032f7bd130be8a83a21443a55784f
Thomas Laporte <thomas.laporte03@gmail.com>
2017-01-20 16:41:52 +0100

    Delete treatment.php

--- treatment.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('bddConnection.php');
-
-function getProducts()
-{
-   $sql = "SELECT * FROM products;";
-   $sth = $dbh->query($sql);
-   $rowset = $sth->fetchAll(PDO::FETCH_ASSOC);
-}
