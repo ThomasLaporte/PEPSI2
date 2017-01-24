cc2ef8d8e2f13e016fad16823e523eeb46fd48e3
Thomas Laporte <thomas.laporte03@gmail.com>
2017-01-24 22:43:36 +0100

    Add files via upload

--- updateProduct.php
+++ updateProduct.php
@@ -25,30 +25,30 @@
 
     if(isset($_GET['product'])){
       $currentProduct = $lstFunctions->getProductByID($_GET['product']);
-      $content = "";
-
+      $content = "<h1>Modification du produit</h1>";
       $content .= "<form method=\"post\">";
-      $content .= "Nom du produit: <input type=\"text\" name=\"productName\" placeholder=\"".  $currentProduct['name']."\"><br>";
-      $content .= "Réference:<input type=\"text\" name=\"productPrice\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
-      $content .= "Description: <input type=\"text\" name=\"productName\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
-      $content .= "Quantité:<input type=\"text\" name=\"productPrice\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
-      $content .= "Poids (en Kg): <input type=\"text\" name=\"productName\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
-      $content .= "Dimensions:<input type=\"text\" name=\"productPrice\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
-      $content .= "Image: <input type=\"file\" name=\"productName\" placeholder=\"".  $currentProduct['name'] ."\"><br>";
+      $content .= "Nom du produit: <input type=\"text\" name=\"productName\" value=\"".  $currentProduct['name']."\"><br>";
+      $content .= "Réference:<input type=\"text\" name=\"productPrice\" value=\"".  $currentProduct['reference'] ."\"><br>";
+      $content .= "Description: <textarea rows=\"4\" cols=\"50\" name=\"productName\">".  $currentProduct['description']."</textarea><br>";
+      $content .= "Quantité:<input type=\"text\" name=\"productPrice\" value=\"".  $currentProduct['quantity'] ."\"><br>";
+      $content .= "Poids (en Kg): <input type=\"text\" name=\"productName\" value=\"".  $currentProduct['weight'] ."\"><br>";
+      $content .= "Dimensions:<input type=\"text\" name=\"productPrice\" value=\"".  $currentProduct['dimension'] ."\"><br>";
+      $content .= "Image: <input type=\"file\" name=\"productName\" value=\"".  $currentProduct['picture'] ."\"><br>";
       $content .= "Fabricant: <select>";
       foreach ($lstFunctions->getManufacturers() as $manufacturer) {
-        $content .= "<option value=".$manufacturer['id_manufacturer'].">".$manufacturer['name']."</option>";
+        $isSelected = "";
+        if($manufacturer['id_manufacturer'] == $currentProduct['manufacturer_id_manufacturer']){$isSelected = "Selected";}
+        $content .= "<option value=\"".$manufacturer['id_manufacturer']."\"".$isSelected.">".$manufacturer['name']."</option>";
       }
       $content .= "</select>";
 
       $content .= "Catégorie: <select>";
-      $test = $lstFunctions->getProductsCategs();
-      print_r($test);
-
       foreach ($lstFunctions->getProductsCategs() as $categProduct) {
-        $content .= "<option value=".!empty($categProduct['id_article_category']).">".$categProduct['name']."</option>";
+        $isSelected = "";
+        if($categProduct['id_article_category'] == $currentProduct['article_category_id_article_category']){$isSelected = "Selected";}
+        $content .= "<option value=\"".!empty($categProduct['id_article_category'])."\"".$isSelected.">".$categProduct['name']."</option>";
       }
-      $content .= "</select>";
+      $content .= "</select><br>";
 
       $content .="<input type=\"submit\" value=\"Submit\">";
       $content .="</form>";
