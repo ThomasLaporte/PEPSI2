b09cd04906dfe1529e0798df7c0ca5041d9cabe2
Thomas Laporte <thomas.laporte03@gmail.com>
2017-02-06 22:19:51 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -205,3 +205,39 @@ CREATE TABLE IF NOT EXISTS `characteristics_assigned` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Data for table `users`
+-- -----------------------------------------------------
+
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `firstname`, `mail`, `phone`) VALUES (1, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'test', 'test', 'test@test.com', '0123456789');
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `firstname`, `mail`, `phone`) VALUES (2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin', 'admin', 'admin@admin.com', '0123456789');
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `firstname`, `mail`, `phone`) VALUES (3, 'tony', '39dfa55283318d31afe5a3ff4a0e3253e2045e43', 'evariste', 'tony', 'tony.evariste@gmail.com', '0123456789');
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `firstname`, `mail`, `phone`) VALUES (4, 'henri', '39dfa55283318d31afe5a3ff4a0e3253e2045e43', 'terrier', 'henri', 'henri.terrier@gmail.com', '0123456789');
+
+
+
+
+-- -----------------------------------------------------
+-- Data for table `categorie_product`
+-- -----------------------------------------------------
+
+INSERT INTO `categorie_product` (`id`, `name`) VALUES (1, 'Enceinte HIFI');
+INSERT INTO `categorie_product` (`id`, `name`) VALUES (2, 'Cables Audio');
+INSERT INTO `categorie_product` (`id`, `name`) VALUES (3, 'Amplificateurs');
+INSERT INTO `categorie_product` (`id`, `name`) VALUES (4, 'Lampes');
+
+
+-- -----------------------------------------------------
+-- Data for table `product`
+-- -----------------------------------------------------
+
+INSERT INTO `product` (`id`, `reference`, `name`, `designation`, `designation_complete`, `weight`, `picture`, `delivery`, `feature`, `Dimension`, `stock`, `categorie_product_id`) VALUES (1, 'ENC02', 'KUB', 'caisson de basse haut de gamme', 'Le caisson de basse KUB est un caisson de basses haut de gamme et ultra performant doté d\'un haut-parleur de 30.5cm et d\'une amplification Ultra-Class-D de 300W RMS montant à 900W en crête !', '33', NULL, 15, 'Puissance de sortie:900', '397x563x594', 20, 1);
+
+-- -----------------------------------------------------
+-- Data for table `price`
+-- -----------------------------------------------------
+
+INSERT INTO `price` (`users_id`, `product_id`, `price`) VALUES (1, 1, 3000);
+INSERT INTO `price` (`users_id`, `product_id`, `price`) VALUES (4, 1, 2000);
