4b2f76098abe9b07ffa099999b96cfebe47125d8
boillatlucas <boillatlucas95@gmail.com>
2017-03-03 12:19:21 +0100

    Update BDD3.sql

--- BDD3.sql
+++ BDD3.sql
@@ -1,17 +1,21 @@
--- MySQL Workbench Forward Engineering
+
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Table `language`
+-- Schema PEPSI2
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `language` ;
+CREATE SCHEMA IF NOT EXISTS `PEPSI2` DEFAULT CHARACTER SET latin1 ;
+USE `PEPSI2` ;
 
-CREATE TABLE IF NOT EXISTS `language` (
-  `idlanguage` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
+-- -----------------------------------------------------
+-- Table `PEPSI2`.`language`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`language` (
+  `idlanguage` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
   PRIMARY KEY (`idlanguage`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
@@ -19,20 +23,18 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `article_category`
+-- Table `PEPSI2`.`article_category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `article_category` ;
-
-CREATE TABLE IF NOT EXISTS `article_category` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`article_category` (
   `id_article_category` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
   `code` INT(11) NOT NULL,
-  `language_idlanguage` INT NOT NULL,
+  `language_idlanguage` INT(11) NOT NULL,
   PRIMARY KEY (`id_article_category`),
   INDEX `fk_article_category_language1_idx` (`language_idlanguage` ASC),
   CONSTRAINT `fk_article_category_language1`
     FOREIGN KEY (`language_idlanguage`)
-    REFERENCES `language` (`idlanguage`)
+    REFERENCES `PEPSI2`.`language` (`idlanguage`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -40,13 +42,10 @@ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
 
-
 -- -----------------------------------------------------
--- Table `manufacturer`
+-- Table `PEPSI2`.`manufacturer`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `manufacturer` ;
-
-CREATE TABLE IF NOT EXISTS `manufacturer` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`manufacturer` (
   `id_manufacturer` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
   `adress` VARCHAR(50) CHARACTER SET 'utf8' NULL DEFAULT NULL,
@@ -60,15 +59,12 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `article`
+-- Table `PEPSI2`.`article`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `article` ;
-
-CREATE TABLE IF NOT EXISTS `article` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`article` (
   `id_article` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
   `reference` VARCHAR(20) CHARACTER SET 'utf8' NULL DEFAULT NULL,
-  `description` LONGTEXT CHARACTER SET 'utf8' NULL DEFAULT NULL,
   `quantity` INT(3) NOT NULL,
   `weight` FLOAT NULL DEFAULT NULL,
   `dimension` VARCHAR(50) CHARACTER SET 'utf8' NULL DEFAULT NULL,
@@ -80,12 +76,12 @@ CREATE TABLE IF NOT EXISTS `article` (
   INDEX `fk_article_article_category1_idx` (`article_category_id_article_category` ASC),
   CONSTRAINT `fk_article_article_category1`
     FOREIGN KEY (`article_category_id_article_category`)
-    REFERENCES `article_category` (`id_article_category`)
+    REFERENCES `PEPSI2`.`article_category` (`id_article_category`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_article_manufacturer1`
     FOREIGN KEY (`manufacturer_id_manufacturer`)
-    REFERENCES `manufacturer` (`id_manufacturer`)
+    REFERENCES `PEPSI2`.`manufacturer` (`id_manufacturer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -94,19 +90,18 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `customer`
+-- Table `PEPSI2`.`customer`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `customer` ;
-
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`customer` (
   `id_customer` INT(11) NOT NULL AUTO_INCREMENT,
   `login` VARCHAR(25) CHARACTER SET 'utf8' NOT NULL,
   `password` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
-  `customer_name` VARCHAR(45) NULL,
+  `customer_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
   `email` VARCHAR(50) CHARACTER SET 'utf8' NULL DEFAULT NULL,
   `name` VARCHAR(25) CHARACTER SET 'utf8' NULL DEFAULT NULL,
   `firstname` VARCHAR(45) CHARACTER SET 'utf8' NULL DEFAULT NULL,
   `phone` INT(20) NULL DEFAULT NULL,
+  `admin` INT(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_customer`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
@@ -114,24 +109,22 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `Price`
+-- Table `PEPSI2`.`Price`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Price` ;
-
-CREATE TABLE IF NOT EXISTS `Price` (
-  `Price` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`Price` (
+  `Price` INT(11) NOT NULL,
   `article_id_article` INT(11) NOT NULL,
   `customer_id_customer` INT(11) NOT NULL,
   PRIMARY KEY (`article_id_article`, `customer_id_customer`),
   INDEX `fk_Price_customer1_idx` (`customer_id_customer` ASC),
   CONSTRAINT `fk_Price_article1`
     FOREIGN KEY (`article_id_article`)
-    REFERENCES `article` (`id_article`)
+    REFERENCES `PEPSI2`.`article` (`id_article`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Price_customer1`
     FOREIGN KEY (`customer_id_customer`)
-    REFERENCES `customer` (`id_customer`)
+    REFERENCES `PEPSI2`.`customer` (`id_customer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -140,110 +133,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `order`
+-- Table `PEPSI2`.`type_adress`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `order` ;
-
-CREATE TABLE IF NOT EXISTS `order` (
-  `id_order` INT(11) NOT NULL AUTO_INCREMENT,
-  `date_purchase` DATE NOT NULL,
-  `customer_id_customer` INT(11) NOT NULL,
-  PRIMARY KEY (`id_order`),
-  INDEX `fk_order_customer1_idx` (`customer_id_customer` ASC),
-  CONSTRAINT `fk_order_customer1`
-    FOREIGN KEY (`customer_id_customer`)
-    REFERENCES `customer` (`id_customer`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `order_detail`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `order_detail` ;
-
-CREATE TABLE IF NOT EXISTS `order_detail` (
-  `idorder_detail` INT NOT NULL,
-  `order_id_order` INT(11) NOT NULL,
-  `article_id_article` INT(11) NOT NULL,
-  `quantity` INT(11) NOT NULL,
-  PRIMARY KEY (`idorder_detail`, `order_id_order`),
-  INDEX `fk_order_detail_article1_idx` (`article_id_article` ASC),
-  INDEX `fk_order_detail_order1_idx` (`order_id_order` ASC),
-  CONSTRAINT `fk_order_detail_article1`
-    FOREIGN KEY (`article_id_article`)
-    REFERENCES `article` (`id_article`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_order_detail_order1`
-    FOREIGN KEY (`order_id_order`)
-    REFERENCES `order` (`id_order`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `characteristics`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `characteristics` ;
-
-CREATE TABLE IF NOT EXISTS `characteristics` (
-  `id_technical` INT(11) NOT NULL AUTO_INCREMENT,
-  `value` FLOAT NULL DEFAULT NULL,
-  `article_id_article` INT(11) NOT NULL,
-  PRIMARY KEY (`id_technical`),
-  INDEX `fk_characteristics_article1_idx` (`article_id_article` ASC),
-  CONSTRAINT `fk_characteristics_article1`
-    FOREIGN KEY (`article_id_article`)
-    REFERENCES `article` (`id_article`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `translate_characteristics`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `translate_characteristics` ;
-
-CREATE TABLE IF NOT EXISTS `translate_characteristics` (
-  `idtranslate_characteristics` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `characteristics_id_technical` INT(11) NOT NULL,
-  `language_idlanguage` INT NOT NULL,
-  PRIMARY KEY (`idtranslate_characteristics`, `language_idlanguage`),
-  INDEX `fk_translate_characteristics_characteristics1_idx` (`characteristics_id_technical` ASC),
-  INDEX `fk_translate_characteristics_language1_idx` (`language_idlanguage` ASC),
-  CONSTRAINT `fk_translate_characteristics_characteristics1`
-    FOREIGN KEY (`characteristics_id_technical`)
-    REFERENCES `characteristics` (`id_technical`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_translate_characteristics_language1`
-    FOREIGN KEY (`language_idlanguage`)
-    REFERENCES `language` (`idlanguage`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `type_adress`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `type_adress` ;
-
-CREATE TABLE IF NOT EXISTS `type_adress` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`type_adress` (
   `id_type_address` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(25) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`id_type_address`))
@@ -253,17 +145,15 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `customer_adress`
+-- Table `PEPSI2`.`customer_adress`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `customer_adress` ;
-
-CREATE TABLE IF NOT EXISTS `customer_adress` (
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`customer_adress` (
   `id_adress` INT(11) NOT NULL AUTO_INCREMENT,
   `adress` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
   `postal_code` INT(11) NOT NULL,
   `city` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
-  `longitude` FLOAT NULL,
-  `latitude` FLOAT NULL,
+  `longitude` FLOAT NULL DEFAULT NULL,
+  `latitude` FLOAT NULL DEFAULT NULL,
   `country` INT(5) NOT NULL,
   `type_adress_id_type_address` INT(11) NOT NULL,
   `customer_id_customer` INT(11) NOT NULL,
@@ -272,17 +162,89 @@ CREATE TABLE IF NOT EXISTS `customer_adress` (
   INDEX `fk_customer_adress_customer1_idx` (`customer_id_customer` ASC),
   CONSTRAINT `fk_customer_adress_type_adress1`
     FOREIGN KEY (`type_adress_id_type_address`)
-    REFERENCES `type_adress` (`id_type_address`)
+    REFERENCES `PEPSI2`.`type_adress` (`id_type_address`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_customer_adress_customer1`
     FOREIGN KEY (`customer_id_customer`)
-    REFERENCES `customer` (`id_customer`)
+    REFERENCES `PEPSI2`.`customer` (`id_customer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PEPSI2`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`order` (
+  `id_order` INT(11) NOT NULL AUTO_INCREMENT,
+  `date_purchase` DATE NOT NULL,
+  `customer_id_customer` INT(11) NOT NULL,
+  PRIMARY KEY (`id_order`),
+  INDEX `fk_order_customer1_idx` (`customer_id_customer` ASC),
+  CONSTRAINT `fk_order_customer1`
+    FOREIGN KEY (`customer_id_customer`)
+    REFERENCES `PEPSI2`.`customer` (`id_customer`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PEPSI2`.`order_detail`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`order_detail` (
+  `idorder_detail` INT(11) NOT NULL AUTO_INCREMENT,
+  `order_id_order` INT(11) NOT NULL,
+  `article_id_article` INT(11) NOT NULL,
+  `quantity` INT(11) NOT NULL,
+  PRIMARY KEY (`idorder_detail`, `order_id_order`),
+  INDEX `fk_order_detail_article1_idx` (`article_id_article` ASC),
+  INDEX `fk_order_detail_order1_idx` (`order_id_order` ASC),
+  CONSTRAINT `fk_order_detail_article1`
+    FOREIGN KEY (`article_id_article`)
+    REFERENCES `PEPSI2`.`article` (`id_article`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_detail_order1`
+    FOREIGN KEY (`order_id_order`)
+    REFERENCES `PEPSI2`.`order` (`id_order`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PEPSI2`.`characteristics`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PEPSI2`.`characteristics` (
+  `id_characteristics` INT(11) NOT NULL AUTO_INCREMENT,
+  `article_id_article` INT(11) NOT NULL,
+  `description` LONGTEXT NOT NULL,
+  `language_idlanguage` INT(11) NOT NULL,
+  PRIMARY KEY (`id_characteristics`, `article_id_article`),
+  INDEX `fk_characteristics_language1_idx` (`language_idlanguage` ASC),
+  INDEX `fk_characteristics_article1_idx` (`article_id_article` ASC),
+  CONSTRAINT `fk_characteristics_language1`
+    FOREIGN KEY (`language_idlanguage`)
+    REFERENCES `PEPSI2`.`language` (`idlanguage`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_characteristics_article1`
+    FOREIGN KEY (`article_id_article`)
+    REFERENCES `PEPSI2`.`article` (`id_article`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
