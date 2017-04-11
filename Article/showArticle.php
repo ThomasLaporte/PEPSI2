c489d88edf7d572745a4b1d0a082298f58250281
Thomas Laporte <thomas.laporte03@gmail.com>
2017-04-11 23:09:11 +0200

    Add files via upload

--- Article/showArticle.php
+++ Article/showArticle.php
@@ -17,14 +17,14 @@
         <h2><?php echo $currentProduct['name']; ?></h2>
         <ul>
             <li>REF : <?php echo $currentProduct['reference']; ?></li>
-            <li>TVA : XXX</li>
-            <li>Prix HT : <?php echo $currentProduct['name']."€"; ?></li>
-            <li>Prix TTC : XXX</li>
+            <li>TVA : 20%</li>
+            <li>Prix HT : <?php echo $currentProduct['public_price']."€"; ?></li>
+            <li>Prix TTC : <?php echo ((intval($currentProduct['public_price']) * 1.20)."€"); ?></li>
             <li>En stock : <?php echo $currentProduct['quantity']; ?></li>
-            <li>Fabriquant : <?php echo $currentProduct['name']; ?></li>
-            <li>Dimension : XXX</li>
-            <li>Poids : XXX</li>
-            <li>Catégorie : <a href="index.php#lampes">XXX</a></li>
+            <li>Fabriquant : <?php echo $lstFunctions->getManufacturerById($currentProduct['manufacturer_id_manufacturer'])['name']; ?></li>
+            <li>Dimension : <?php echo $currentProduct['dimension']; ?></li>
+            <li>Poids : <?php echo $currentProduct['weight']; ?></li>
+            <li>Catégorie : <a href="../CategArticle/showArticleCateg.php?categ=<?php echo $currentProduct['article_category_code']; ?>"><?php echo $lstFunctions->getProductCategByID($currentProduct['article_category_code'])[0]['name']; ?></a></li>
         </ul>
         <a href="ajout.php"><i class="fa fa-times fa-2x a_delete" aria-hidden="true"></i></a>
     </article>
@@ -32,9 +32,9 @@
     <article class="article_products_display">
         <a class="article_img_link"><img src="http://lorempixel.com/output/city-q-c-250-175-4.jpg" /></a>
         <div class="article_products_display_details">
-            <p class="article_products_display_details_title">Nom article</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper dignissim ligula sit amet pharetra. Praesent ullamcorper ex in justo condimentum tincidunt. Aliquam fermentum eros vel velit dapibus volutpat. Aliquam mollis libero a tincidunt tincidunt. Maecenas molestie lorem urna, sed vestibulum est sagittis non. Praesent viverra eu nunc id fringilla. Vestibulum ut tempor elit. Nunc facilisis vel arcu efficitur vestibulum. In semper rhoncus felis quis ullamcorper. Morbi rhoncus nec sem eget convallis. Maecenas scelerisque sagittis leo, gravida vulputate eros mollis ut.</p>
-            <p>Aliquam sed nisi dignissim, venenatis dui nec, laoreet metus. Vestibulum blandit purus nec neque vehicula lobortis. Praesent dignissim condimentum ex eu interdum. Donec at iaculis urna. Maecenas non augue lacus. Nam id purus in urna tristique posuere. Nullam eleifend, orci eu tincidunt vulputate, ante mauris commodo nisl, eu elementum metus lacus id magna. Pellentesque quis cursus magna. Suspendisse fermentum enim non porttitor tristique. Sed finibus arcu enim, a ultrices mauris auctor sit amet.</p>
+            <p class="article_products_display_details_title"><?php echo $currentProduct['name']; ?></p>
+            <p><?php echo $lstFunctions->getCharacteristicsByIdArticle($currentProduct['id_article'])[0]['description']; ?></p>
+            <p><?php echo $lstFunctions->getCharacteristicsByIdArticle($currentProduct['id_article'])[0]['specification']; ?></p>
         </div>
     </article>
     </div>
