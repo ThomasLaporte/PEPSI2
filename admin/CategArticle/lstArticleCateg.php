4ac99f0186feff398b74f97ca2ed3d83bc352c60
Thomas Laporte <thomas.laporte03@gmail.com>
2017-05-17 09:53:34 +0200

    Add files via upload

--- admin/CategArticle/lstArticleCateg.php
+++ admin/CategArticle/lstArticleCateg.php
@@ -1,17 +1,15 @@
 <?php
+$title_page = 'Spectasonic - Catalogue Backoffice';
 require_once "../../header.php";
 include "../../Class/Functions.php";
+$lstFunctions = new Functions();
 
-
-
-  $content = "";
-  $lstFunctions = new Functions();
-
-  $content .= "<br><h3>List of article categories</h3>";
+  $content = "<br><h3>Liste des catégories</h3>";
   $content .=  "<table>";
 
   $content .=  "<tr>";
-    $content .=  "<th>Category name</th>";
+    $content .=  "<th></th>";
+    $content .=  "<th>Nom catégorie</th>";
     $content .=  "<th></th>";
   $content .=  "</tr>";
 
@@ -19,16 +17,14 @@ include "../../Class/Functions.php";
   // Le parametre correspond a la langue : par defaut Francais (id 1)
   foreach ($lstFunctions->getProductsCategsByLang('1') as $categ) {
     $content .= "<tr>";
-      $content .= "<td><a href=\"deleteCategArticle.php?categ=".$categ['code']."\">-</a></td>";
+      $content .= "<td><a href=\"deleteCategArticle.php?categ=".$categ['code']."\"> X </a></td>";
       $content .= "<td>". $categ['name']."</td>";
       $content .= "<td><a href=updateProductCateg.php?categ=".$categ['code'].">Modifier</a></td>";
     $content .= "</tr>";
   }
 
   $content .=  "</table>";
-  $content .= "<a href=addCategArticle.php>Ajouter produit</a>";
-
-
+  $content .= "<a href=addCategArticle.php><input type=\"submit\" class=\"upload-submit\" value=\"Ajouter catégorie\"></a>";
 
   echo $content;
   ?>
--- admin/CategArticle/updateProductCateg.php
+++ admin/CategArticle/updateProductCateg.php
@@ -1,47 +1,60 @@
-
 <?php include "../../Class/Functions.php";
-
+    $title_page = 'Spectasonic - Modifier';
     require_once "../../header.php";
+
     $lstFunctions = new Functions();
     $currentCateg = $lstFunctions->getProductCategByID($_GET['categ']);
 
     // Si le id passé dans $_GET est valide
     if(isset($_GET['categ']) && !empty($currentCateg)){
 
-      if(isset($_POST['submit']) && $_POST['submit'])
-      {
-
-        // Parcours des champs pour enregistrer les modifications
+        // Vérification des champs avant update
+        $validUpdate = 0;
         foreach ($currentCateg as $categ) {
-          // if(isset($_POST["categName". $categ['code']]) && strlen($_POST["categName". $categ['code']]) > 0)
-          // {
-
-            $test = $lstFunctions->updateProductCateg($_GET['categ'], $_POST["categName". $categ['language_idlanguage']], $categ['language_idlanguage'], $_POST["categDesc". $categ['language_idlanguage']]);
-        //  }
-
-
+          if(isset($_POST["categName". $categ['language_id_language']]) && strlen($_POST["categName". $categ['language_id_language']]) > 0)
+          {
+            $validUpdate++;
+          }
         }
-        ?> <script>alert("Mise(s) à jour terminée(s) ! ");
-        //window.location.reload();</script> <?php
-        header("Location: lstArticleCateg.php");
 
+      // SI tous les champs NOM d'une catégorie sont OK --> UPDATE
+      if($validUpdate == count($currentCateg))
+      {
+        foreach ($currentCateg as $categ) {
+            $lstFunctions->updateProductCateg($_GET['categ'], $_POST["categName". $categ['language_id_language']], $categ['language_id_language'], $_POST["categDesc". $categ['language_id_language']]);
+       }
+       header("Location: lstArticleCateg.php");
+      }
+      else {
+        ?> <script>alert("Au moins un champs NOM d'une catégorie n'est pas correctement saisie ! ");</script> <?php
       }
 
-      $content = "<br><h2>Modification de la catégorie</h2>";
-      $content .= "<form method=\"post\">";
+    ?>
+      <form method="post">
+        <div class="wrap">
 
-      foreach ($currentCateg as $categ) {
+        <h1 class="wow fadeIn">Mise à jour d'une catégorie</h1>
 
-        $currentLanguage = $lstFunctions->getLanguageByIdBis($categ['language_idlanguage']);
-        $content .= $currentLanguage['name'] ." name: <input type=\"text\" name=\"categName". $categ['language_idlanguage']."\" value=\"". $categ['name']."\"><br>";
-        $content .= "Description:<textarea rows=\"6\" cols=\"50\" name=\"categDesc". $categ['language_idlanguage']."\">".$categ['description']."</textarea><br><br>";
-      }
-      $content .="<input type=\"submit\" name=\"submit\" value=\"Valider\">";
-      $content .="</form>";
-      echo $content;
+          <section class="catalogue_products wow fadeInUp">
+              <div class="catalogue_products_wrap back">
+            <?php foreach ($currentCateg as $categ) {
+                    $currentLanguage = $lstFunctions->getLanguageByIdBis($categ['language_id_language']);?>
+                    <article class="article_products_display">
+                        <div class="article_products_display_details">
+                            <p class="article_products_display_details_title"><?php echo $currentLanguage['name'];?> name :</p>
+                            <input name="categName<?php echo $categ['language_id_language'];?>" class="back_input_name" type="text" value="<?php echo $categ['name'];?>">
+                            <p class="article_products_display_details_title"><?php echo $currentLanguage['name'];?> description :</p>
+                            <textarea name="categDesc<?php echo $categ['language_id_language'];?>"><?php echo $categ['description'];?></textarea>
+                        </div>
+                    </article>
+              <?php } ?>
+                <button>Modifier</button>
+              </div>
+          </section>
+        </div>
+      </form>
+      <?php
     }
   else {
     header("Location: lstArticleCateg.php");
   } ?>
-  </body>
-</html>
